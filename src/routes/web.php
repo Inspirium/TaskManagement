357e30ed94a9de2bb1baee92725fd8354d123a5a
Marko Banusic <mbanusic@gmail.com>
2017-04-25 14:31:31 +0200

    Initial commit

---
+++ src/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'Inspirium\TaskManagement\Controllers', 'middleware' => ['web', 'auth'], 'prefix' => 'tasks'], function() {
+    Route::get('user/tasks', function() {
+        return view(config('app.template' . '::tasks.user_tasks'));
+    });
+});
