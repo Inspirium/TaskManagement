6e669f0ef3bab5e1a6a18549d2016a1607703187
Marko Banusic <mbanusic@gmail.com>
2017-07-18 10:18:45 +0200

    Temp fix to checkout templates

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -8,13 +8,11 @@ use Inspirium\TaskManagement\Models\Task;
 class TaskController extends Controller {
 
 	public function showTask($id) {
-		$task = Task::firstOrFail($id);
-		return view(config('app.template') . '::tasks.task_details', ['task' => $task]);
+		return view(config('app.template') . '::tasks.task_details');
 	}
 
 	public function editTask($id = null) {
-		$task = Task::firstOrNew($id);
-		return view(config('app.template') . '::tasks.task_new', ['task' => $task]);
+		return view(config('app.template') . '::tasks.task_new');
 	}
 
 	public function showTasks() {
