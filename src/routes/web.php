2544e405d9d937fbe09fda66c3599279246a3b5e
Marko Banusic <mbanusic@gmail.com>
2017-07-18 14:39:29 +0200

    Task in Vue component

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Inspirium\TaskManagement\Controllers', 'middleware' => ['web', 'auth']], function() {
 
 	Route::group(['prefix' => 'tasks'], function() {
-		Route::get('/', 'TaskController@showTasks');
+		Route::any('/', function() {
+			return view(config('app.template') . '::router-view');
+		});
 	});
 
 	Route::group(['prefix' => 'task'], function() {
