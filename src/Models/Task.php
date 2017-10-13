cb04bcb4d189a9ed809bdd2ed9bb99cc48fdb068
Marko Banusic <mbanusic@gmail.com>
2017-10-14 01:24:06 +0200

    Docs for models

--- src/Models/Task.php
+++ src/Models/Task.php
@@ -6,18 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
- * Class Task
- * @package Inspirium\TaskManagement\Models
+ * Inspirium\TaskManagement\Models\Task
  *
- * @property $id
- * @property $name
- * @property $type
- * @property $description
- * @property $priority
- * @property $status
- * @property $assigner_id
- * @property $related
- * @property $deadline
+ * @property int $id
+ * @property string|null $name
+ * @property string|null $type
+ * @property string|null $description
+ * @property string|null $priority
+ * @property string|null $status
+ * @property int|null $assigner_id
+ * @property int|null $related_id
+ * @property string|null $related_type
+ * @property string|null $deadline
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \Inspirium\HumanResources\Models\Employee|null $assigner
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\HumanResources\Models\Department[] $departments
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\FileManagement\Models\Document[] $documents
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\HumanResources\Models\Employee[] $employees
+ * @property-read mixed $related_link
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $related
+ * @method static bool|null forceDelete()
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\TaskManagement\Models\Task onlyTrashed()
+ * @method static bool|null restore()
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereAssignerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereDeadline($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task wherePriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereRelatedId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereRelatedType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\TaskManagement\Models\Task whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\TaskManagement\Models\Task withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|\Inspirium\TaskManagement\Models\Task withoutTrashed()
+ * @mixin \Eloquent
  */
 class Task extends Model {
 
